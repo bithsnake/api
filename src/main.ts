8bb3fea1ff1f03da0f951d3725d7897a658480b1
Kimmo Savilampi <kimmo.savilampi@ariasoft.se>
2026-03-16 14:20:37 +0100

    refactor: update to Fastify platform and adjust TypeScript configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fastify from 'fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+export const app = fastify();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(
+    process.env.PORT ? Number(process.env.PORT) : 3000,
+    '0.0.0.0',
+  );
 }
-bootstrap();
+
+void bootstrap();
